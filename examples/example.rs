65c9617693bebce6757a62c7c8a6113e57a319b5
MashPlant <740678788@qq.com>
2020-04-13 19:07:21 +0800

    add downcast

--- examples/example.rs
+++ examples/example.rs
@@ -7,33 +7,35 @@ inheritance::inheritance!(
     pub circumference: f32
   }
 
-  pub struct Circle : Shape {
+  pub struct Circle: Shape {
     pub(crate) radius: f32
   }
 
-  pub struct Rect : Shape {
+  pub struct Rect: Shape {
     width: f32,
     height: f32
   }
 
-  pub struct Triangle : Shape {
+  pub struct Triangle: Shape {
     a: f32,
     b: f32,
     c: f32
   }
 
-  pub struct CircleA : Circle {
+  pub struct CircleA: Circle {
     data: u32,
   }
 
-  pub struct CircleB : Circle {
+  pub struct CircleB: Circle {
     data: u64,
   }
 
-  pub struct RectX : Rect {}
+  pub struct RectX: Rect {}
 
-  pub struct RectY : RectX {}
+  pub struct RectY: RectX {}
 );
 
-
-fn main() {}
+fn main() {
+  // use std::f32::consts::PI;
+  // let c = CircleA::new(Circle {  });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,30 +7,30 @@ use std::collections::HashMap;
 
 #[proc_macro]
 pub fn inheritance(input: TokenStream) -> TokenStream {
-  let mut structs = parse_macro_input!(input as ItemStructs).0;
+  let mut ss = parse_macro_input!(input as Itemss).0;
   let mut ident2id = HashMap::new();
-  for (idx, s) in structs.iter().enumerate() {
-    // if insert `&s.ident` into `ident2id`, it will borrow `structs`
+  for (idx, s) in ss.iter().enumerate() {
+    // if insert `&s.ident` into `ident2id`, it will borrow `ss`
     // which disallows following mutation of other fields
     if ident2id.insert(s.ident.clone(), idx).is_some() {
       panic!("duplicate struct name `{}`", s.ident);
     }
   }
-  for idx in 0..structs.len() {
-    if let Some(p) = &structs[idx].parent {
+  for idx in 0..ss.len() {
+    if let Some(p) = &ss[idx].parent {
       if let Some(&p_idx) = ident2id.get(p) {
-        structs[idx].parent_idx = Some(p_idx);
-        structs[p_idx].children_idx.push(idx);
-      } else { panic!("`{}`'s parent `{}` not found", structs[idx].ident, p); }
+        ss[idx].parent_idx = Some(p_idx);
+        ss[p_idx].direct_ch_idx.push(idx);
+      } else { panic!("`{}`'s parent `{}` not found", ss[idx].ident, p); }
     }
   }
   // detect cyclic inheritance
-  for idx in 0..structs.len() {
-    let mut s = &mut structs[idx];
+  for idx in 0..ss.len() {
+    let mut s = &mut ss[idx];
     loop {
       if s.visit_time == 0 {
         s.visit_time = idx + 1;
-        if let Some(p) = s.parent_idx { s = &mut structs[p]; } else { break; }
+        if let Some(p) = s.parent_idx { s = &mut ss[p]; } else { break; }
       } else {
         if s.visit_time == idx + 1 { panic!("cyclic inheritance"); }
         break;
@@ -38,33 +38,37 @@ pub fn inheritance(input: TokenStream) -> TokenStream {
     }
   }
   // since there is no cyclic inheritance, there must be at least a struct with 0 parent
-  for idx in 0..structs.len() {
-    fn dfs(structs: &mut Vec<ItemStruct>, idx: usize, discriminant: &mut usize) {
-      for idx1 in 0..structs[idx].children_idx.len() {
-        let ch_idx = structs[idx].children_idx[idx1];
-        if structs[ch_idx].children_idx.is_empty() {
-          structs[idx].concrete_children_idx.push(ch_idx);
-          structs[ch_idx].discriminant = *discriminant;
-          *discriminant += 1;
+  for idx in 0..ss.len() {
+    fn dfs(ss: &mut Vec<ItemStruct>, idx: usize, d: &mut usize) {
+      for idx1 in 0..ss[idx].direct_ch_idx.len() {
+        let ch_idx = ss[idx].direct_ch_idx[idx1];
+        if ss[ch_idx].direct_ch_idx.is_empty() {
+          ss[idx].concrete_ch_idx.push(ch_idx);
+          ss[ch_idx].discriminant = *d;
+          *d += 1;
         } else {
-          dfs(structs, ch_idx, discriminant);
-          let mut tmp = structs[ch_idx].concrete_children_idx.clone();
-          structs[idx].concrete_children_idx.append(&mut tmp);
+          dfs(ss, ch_idx, d);
+          let mut tmp = ss[ch_idx].concrete_ch_idx.clone();
+          ss[idx].concrete_ch_idx.append(&mut tmp);
+          ss[idx].abstract_ch_idx.push(ch_idx);
+          let mut tmp = ss[ch_idx].abstract_ch_idx.clone();
+          ss[idx].abstract_ch_idx.append(&mut tmp);
         }
       }
     }
-    if structs[idx].parent_idx.is_none() {
-      let mut discriminant = 0;
-      dfs(&mut structs, idx, &mut discriminant);
-      if discriminant == 0 {
-        panic!("no concrete subclass found for `{}`", structs[idx].ident);
+    if ss[idx].parent_idx.is_none() {
+      let mut d = 0;
+      dfs(&mut ss, idx, &mut d);
+      if d == 0 {
+        panic!("no concrete subclass found for `{}`", ss[idx].ident);
       }
     }
   }
-  let it = structs.iter().map(
-    |ItemStruct { vis, ident, parent, fields, children_idx, concrete_children_idx, discriminant, .. }| {
-      if children_idx.is_empty() {
-        let p = if let Some(p) = parent { p } else { panic!("struct `{}` is isolated from other structs", ident) };
+  let it = ss.iter().map(
+    |ItemStruct { vis, ident, parent, fields, direct_ch_idx, concrete_ch_idx, abstract_ch_idx, discriminant, .. }| {
+      if direct_ch_idx.is_empty() {
+        let p = if let Some(p) = parent { p } else { panic!("struct `{}` is isolated from other ss", ident) };
+        let deref_impl = deref_impl(ident, p);
         let enum_ident = Ident::new(&(ident.to_string() + &p.to_string()), ident.span());
         let p_enum_ident = Ident::new(&("Generic".to_string() + &p.to_string()), ident.span());
         let fields_names = fields.iter().map(|f| &f.ident);
@@ -75,14 +79,7 @@ pub fn inheritance(input: TokenStream) -> TokenStream {
             #fields
           }
 
-          impl ::core::ops::Deref for #ident {
-            type Target = #p;
-            fn deref(&self) -> &Self::Target { &self.base }
-          }
-
-          impl ::core::ops::DerefMut for #ident {
-            fn deref_mut(&mut self) -> &mut Self::Target { &mut self.base }
-          }
+          #deref_impl
 
           #[repr(C, usize)]
           #vis enum #enum_ident { #ident(#ident) = #discriminant }
@@ -91,7 +88,6 @@ pub fn inheritance(input: TokenStream) -> TokenStream {
             pub fn new(base: #p, #fields) -> #enum_ident {
               #enum_ident::#ident(#ident { base, #(#fields_names),* })
             }
-
             pub fn upcast(&self) -> &#p_enum_ident { unsafe { ::core::mem::transmute(self) } }
           }
 
@@ -99,20 +95,54 @@ pub fn inheritance(input: TokenStream) -> TokenStream {
             type Target = #ident;
             fn deref(&self) -> &Self::Target { match self { #enum_ident::#ident(x) => x } }
           }
-
           impl ::core::ops::DerefMut for #enum_ident {
             fn deref_mut(&mut self) -> &mut Self::Target { match self { #enum_ident::#ident(x) => x } }
           }
         }
       } else {
         let enum_ident = Ident::new(&("Generic".to_string() + &ident.to_string()), ident.span());
-        let variants = concrete_children_idx.iter().map(|&idx| {
-          let (ident, discriminant) = (&structs[idx].ident, structs[idx].discriminant);
-          quote! { #ident(#ident) = #discriminant, }
+        let variants = concrete_ch_idx.iter().map(|&idx| {
+          let (ident, d) = (&ss[idx].ident, ss[idx].discriminant);
+          quote! { #ident(#ident) = #d, }
         });
+        let base = parent.as_ref().map(|p| quote! { base: #p, });
+        let deref_impl = parent.as_ref().map(|p| deref_impl(ident, p));
+        let upcast = parent.as_ref().map(|p| {
+          let p_enum_ident = Ident::new(&("Generic".to_string() + &p.to_string()), p.span());
+          quote! { pub fn upcast(&self) -> &#p_enum_ident { unsafe { ::core::mem::transmute(self) } } }
+        });
+        let trait_ident = Ident::new(&(ident.to_string() + "Info"), ident.span());
+        // the trait must be unsafe because one can implement it on other types without unsafe code easily
+        // which makes `downcast` / `downcast_mut` able to cast into these types, which is unsafe
+        let trait_def = if let Some(p) = parent {
+          let p_trait_ident = Ident::new(&(p.to_string() + "Info"), p.span());
+          quote! { pub unsafe trait #trait_ident: #p_trait_ident {} }
+        } else {
+          quote! { pub unsafe trait #trait_ident { fn classof(d: usize) -> bool; } }
+        };
+        let trait_impl = if parent.is_some() {
+          let it = concrete_ch_idx.iter().chain(abstract_ch_idx.iter()).map(|&idx| &ss[idx].ident);
+          quote! { #(unsafe impl #trait_ident for #it {})* }
+        } else {
+          let it1 = concrete_ch_idx.iter().map(|&idx| {
+            let (ident, d) = (&ss[idx].ident, ss[idx].discriminant);
+            quote! { unsafe impl #trait_ident for #ident { fn classof(d: usize) -> bool { d == #d } } }
+          });
+          let it2 = abstract_ch_idx.iter().map(|&idx| {
+            let ident = &ss[idx].ident;
+            let ds = ss[idx].concrete_ch_idx.iter().map(|&idx| ss[idx].discriminant);
+            quote! { unsafe impl #trait_ident for #ident { fn classof(d: usize) -> bool { #(d == #ds) ||* } } }
+          });
+          quote! { #(#it1)* #(#it2)* }
+        };
         quote! {
           #[repr(C)]
-          #vis struct #ident { #fields }
+          #vis struct #ident {
+            #base
+            #fields
+          }
+
+          #deref_impl
 
           #[repr(C, usize)]
           #vis enum #enum_ident { #(#variants)* }
@@ -123,12 +153,24 @@ pub fn inheritance(input: TokenStream) -> TokenStream {
               unsafe { &*((self as *const _ as *const u8).add(::core::mem::size_of::<usize>()) as *const _) }
             }
           }
-
           impl ::core::ops::DerefMut for #enum_ident {
             fn deref_mut(&mut self) -> &mut Self::Target {
               unsafe { &mut *((self as *mut _ as *mut u8).add(::core::mem::size_of::<usize>()) as *mut _) }
             }
           }
+
+          impl #enum_ident {
+            #upcast
+            pub fn downcast<T: #trait_ident>(&self) -> Option<&T> {
+              unsafe { if T::classof(*(self as *const _ as *const usize)) { Some(unsafe { ::core::mem::transmute(self) }) } else { None } }
+            }
+            pub fn downcast_mut<T: #trait_ident>(&mut self) -> Option<&mut T> {
+              unsafe { if T::classof(*(self as *const _ as *const usize)) { Some(::core::mem::transmute(self)) } else { None } }
+            }
+          }
+
+          #trait_def
+          #trait_impl
         }
       }
     });
@@ -144,9 +186,10 @@ struct ItemStruct {
   fields: Punctuated<Field, Token![,]>,
   // below fields are synthetic
   parent_idx: Option<usize>,
-  children_idx: Vec<usize>,
+  direct_ch_idx: Vec<usize>,
   visit_time: usize,
-  concrete_children_idx: Vec<usize>,
+  concrete_ch_idx: Vec<usize>,
+  abstract_ch_idx: Vec<usize>,
   discriminant: usize,
 }
 
@@ -159,16 +202,28 @@ impl Parse for ItemStruct {
     let parent = if input.parse::<Token![:]>().is_ok() { Some(input.parse()?) } else { None };
     let _ = braced!(content in input);
     let fields = content.parse_terminated(Field::parse_named)?;
-    Ok(ItemStruct { vis, ident, parent, fields, parent_idx: None, children_idx: Vec::new(), visit_time: 0, concrete_children_idx: Vec::new(), discriminant: 0 })
+    Ok(ItemStruct { vis, ident, parent, fields, parent_idx: None, direct_ch_idx: Vec::new(), visit_time: 0, concrete_ch_idx: Vec::new(), abstract_ch_idx: Vec::new(), discriminant: 0 })
   }
 }
 
-struct ItemStructs(Vec<ItemStruct>);
+struct Itemss(Vec<ItemStruct>);
 
-impl Parse for ItemStructs {
+impl Parse for Itemss {
   fn parse(input: ParseStream) -> Result<Self> {
     let mut ret = Vec::new();
     while !input.is_empty() { ret.push(input.parse()?); }
-    Ok(ItemStructs(ret))
+    Ok(Itemss(ret))
+  }
+}
+
+fn deref_impl(ident: &Ident, p: &Ident) -> proc_macro2::TokenStream {
+  quote! {
+    impl ::core::ops::Deref for #ident {
+      type Target = #p;
+      fn deref(&self) -> &Self::Target { &self.base }
+    }
+    impl ::core::ops::DerefMut for #ident {
+      fn deref_mut(&mut self) -> &mut Self::Target { &mut self.base }
+    }
   }
 }
